0616757dfec54c3a59bcb543ae69ad106cc5389e
Denis Gindulin <denisgindulin@me.com>
2023-06-09 15:34:55 +0300

    added an alert for the invalid user name or password situation

--- AboutMeApp/LogInViewController.swift
+++ AboutMeApp/LogInViewController.swift
@@ -9,26 +9,33 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    //MARK: - IBOutlets
     @IBOutlet var userNameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     
     @IBOutlet var nameForgotButton: UIButton!
     @IBOutlet var passwordForgotButton: UIButton!
+    @IBOutlet var logInButton: UIButton!
     
-    
-    let userName = "User"
+    //MARK: - Private Properties
+    private let userName = "User"
     private let userPassword = "Password"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        passwordTextField.isSecureTextEntry = true
+    }
+
+    //MARK: - IBActions
+    @IBAction func logInButtonWasPressed() {
+        if userNameTextField.text == userName && passwordTextField.text == userPassword {
+            //TODO: - здесь надо сделать переход на второй экран с приветствием по имени и с кнопкой Log Out
+        } else {
+            showInvalidLogInAlert()
+        }
+        
     }
     
-
-    //MARK: - Private Methods
-    @IBAction func logInButton() {
-        //TODO: - здесь надо сделать переход на второй экран с приветствием по имени и с кнопкой Log Out
-    }
-
     @IBAction func forgotButtonWasPressed(_ sender: UIButton) {
         switch sender {
         case nameForgotButton:
@@ -42,18 +49,33 @@ class LogInViewController: UIViewController {
 }
 
 //MARK: - UIAlertControllers
-extension LogInViewController {
+private extension LogInViewController {
     
-    private func showForgotAlert(withMessage message: String) {
+    func showForgotAlert(withMessage message: String) {
         let forgotAlert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         forgotAlert.title = "Oops!"
-        let okAction = UIAlertAction(
-            title: "OK",
-            style: .default
-        )
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
         forgotAlert.addAction(okAction)
         
         present(forgotAlert, animated: true)
+    }
+    
+    func showInvalidLogInAlert() {
+        let invalidLogInAlert = UIAlertController(
+            title: "Invalid login or password",
+            message: "Please, enter correct login and password",
+            preferredStyle: .alert
+        )
+    
+        let okAction = UIAlertAction(title: "OK", style: .default) {_ in self.clearTextField()}
+        invalidLogInAlert.addAction(okAction)
+        
+        present(invalidLogInAlert, animated: true)
+    }
+    
+    func clearTextField() {
+        passwordTextField.text = ""
     }
 }
         
